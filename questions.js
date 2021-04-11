e384b3e5257029cc07e4cb50eab37212fa7a8f9a
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-04-11 18:05:09 -0400

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,14 @@
+var questionBank = [
+      {
+        question:
+          "5+5",
+        option: ["11", "7", "10", "2"],
+        answer: "10",
+      },
+      {
+        question:
+          "4+3",
+        option: ["8", "6", "7", "5"],
+        answer: "7",
+      },
+    ]
